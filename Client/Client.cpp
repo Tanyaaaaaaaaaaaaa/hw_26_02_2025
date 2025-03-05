9c5bb3448ec50cf7c4c632d614f4c8fda244df5b
Tanyaaaaaaaaaaaaa <167525210+Tanyaaaaaaaaaaaaa@users.noreply.github.com>
2025-03-05 12:29:27 +0200

    Add files via upload

---
+++ Client/Client.cpp
@@ -0,0 +1,110 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <iostream>
+#include <windows.h>
+// #include <winsock2.h>
+#include <ws2tcpip.h>
+using namespace std;
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+#define PAUSE 0
+
+int main(int argc, char** argv)
+{
+    system("title CLIENT SIDE");
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0)
+    {
+        return 11;
+    }
+
+    addrinfo hints;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    const char* ip = "localhost";
+    addrinfo* result = NULL;
+    iResult = getaddrinfo(ip, DEFAULT_PORT, &hints, &result);
+
+    if (iResult != 0)
+    {
+        WSACleanup();
+        return 12;
+    }
+
+    SOCKET ConnectSocket = INVALID_SOCKET;
+    for (addrinfo* ptr = result; ptr != NULL; ptr = ptr->ai_next)
+    {
+        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (ConnectSocket == INVALID_SOCKET)
+        {
+            WSACleanup();
+            return 13;
+        }
+
+        iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == SOCKET_ERROR)
+        {
+            closesocket(ConnectSocket);
+            ConnectSocket = INVALID_SOCKET;
+            continue;
+        }
+        break;
+    }
+
+    freeaddrinfo(result);
+    if (ConnectSocket == INVALID_SOCKET)
+    {
+        cout << "It is impossible to connect to the server . Check if the server process is running !\n";
+        WSACleanup();
+        return 14;
+    }
+    else
+    {
+        cout << "Connection to the server was successful !\n";
+    }
+
+    while (true)
+    {
+        char message[DEFAULT_BUFLEN];
+        cout << "Enter your number : ";
+        cin.getline(message, DEFAULT_BUFLEN);
+        iResult = send(ConnectSocket, message, strlen(message), 0);
+        if (iResult == SOCKET_ERROR)
+        {
+            closesocket(ConnectSocket);
+            WSACleanup();
+            return 15;
+        }
+
+        char answer[DEFAULT_BUFLEN];
+        iResult = recv(ConnectSocket, answer, DEFAULT_BUFLEN, 0);
+        answer[iResult] = '\0';
+
+        if (iResult > 0)
+        {
+            cout << "The server process sent a response : " << answer << "\n";
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    iResult = shutdown(ConnectSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR)
+    {
+        closesocket(ConnectSocket);
+        WSACleanup();
+        return 16;
+    }
+
+    closesocket(ConnectSocket);
+    WSACleanup();
+    cout << "The client process completes it`s work !\n";
+    return 0;
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,130 @@
+﻿#define WIN32_LEAN_AND_MEAN 
+#include <iostream>
+#include <windows.h>
+// #include <winsock2.h>
+#include <ws2tcpip.h>
+#include <cstdlib>
+using namespace std;
+#pragma comment (lib, "Ws2_32.lib")
+// #pragma comment (lib, "Mswsock.lib")
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015" 
+#define PAUSE 0
+
+int main()
+{
+    system("title SERVER SIDE");
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    struct addrinfo hints;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    struct addrinfo* result = NULL;
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    if (iResult != 0)
+    {
+        WSACleanup();
+        return 2;
+    }
+
+    SOCKET ListenSocket = INVALID_SOCKET;
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET)
+    {
+        freeaddrinfo(result);
+        WSACleanup();
+        return 3;
+    }
+
+    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR)
+    {
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 4;
+    }
+
+    freeaddrinfo(result);
+    iResult = listen(ListenSocket, SOMAXCONN);
+    if (iResult == SOCKET_ERROR)
+    {
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 5;
+    }
+    else
+    {
+        cout << "Listening to information from the client begins . Please launch the client !\n";
+    }
+
+    SOCKET ClientSocket = INVALID_SOCKET;
+    ClientSocket = accept(ListenSocket, NULL, NULL);
+    if (ClientSocket == INVALID_SOCKET)
+    {
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 6;
+    }
+    else
+    {
+        cout << "The connection with the client program has been established successfully !\n";
+    }
+
+    closesocket(ListenSocket);
+    do
+    {
+        char message[DEFAULT_BUFLEN];
+        iResult = recv(ClientSocket, message, DEFAULT_BUFLEN, 0);
+        if (iResult > 0)
+        {
+            message[iResult] = '\0';
+            cout << "The client process sent a message : " << message << "\n";
+
+            if (strcmp(message, "exit") == 0) break;
+            int receivedNumber = atoi(message);
+            receivedNumber++;
+            char answer[DEFAULT_BUFLEN];
+            _itoa_s(receivedNumber, answer, 10);
+            cout << "The server process sends a response : " << answer << "\n";
+            int iSendResult = send(ClientSocket, answer, (int)strlen(answer), 0);
+
+            if (iSendResult == SOCKET_ERROR)
+            {
+                closesocket(ClientSocket);
+                WSACleanup();
+                return 7;
+            }
+        }
+        else if (iResult == 0)
+        {
+            cout << "The connection is closed\n";
+        }
+        else
+        {
+            closesocket(ClientSocket);
+            WSACleanup();
+            return 8;
+        }
+    } while (iResult > 0);
+
+    iResult = shutdown(ClientSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR)
+    {
+        closesocket(ClientSocket);
+        WSACleanup();
+        return 9;
+    }
+    else
+    {
+        cout << "Server process stops working\n";
+    }
+
+    closesocket(ClientSocket);
+    WSACleanup();
+    return 0;
+}
